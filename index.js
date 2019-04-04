cf22c92feba5a460f59605045d4f8ec565de5230
Michael Hicks <kustomdesigner@Michaels-MBP.lan1>
2019-04-04 17:12:47 -0400

    fixing merge conflict

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const url = require('url');
 
 
 // The server should respond to all requests with a string
-const server = http.createServer(function(req, res){
+const server = http.createServer(function(req, res) {
 
     // Get the url and parse it
     var parsedUrl = url.parse(req.url, true);
@@ -16,18 +16,25 @@ const server = http.createServer(function(req, res){
     // Get the path
     var path = parsedUrl.pathname;
     var trimmedPath = path.replace(/^\/+|\/+$/g, '');
-    
+
+    // Get the query string object
+    var queryStringObject = parsedUrl.query; 
+
+    // Get the http method
+    var method = req.method.toLowerCase();
+
+    // Get the headers as an object
+    var headers = req.headers;
+
     // Send the response
     res.end('Hello World\n');
 
     // Log the request path
-    console.log(
-        'Request received on path: '+trimmedPath
-    );
+    console.log('Request received with these headers', headers);
 });
 
 // Start the server and have it listen on port 3000
-server.listen(3000, function(){
+server.listen(3000, function() {
     console.log('The server is listening on port 3000 now');
 });
 
