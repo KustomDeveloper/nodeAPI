aa909dbb1b0f1f41b4938c62bcd3db1e06699b46
Michael Hicks <kustomdesigner@Michaels-MBP.lan1>
2019-04-04 21:11:12 -0400

    Setup payload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
-
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string
 const server = http.createServer(function(req, res) {
@@ -26,11 +26,21 @@ const server = http.createServer(function(req, res) {
     // Get the headers as an object
     var headers = req.headers;
 
-    // Send the response
-    res.end('Hello World\n');
+    // Get the payload if any
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', function(data) {
+        buffer += decoder.write(data);
+    });
+    req.on('end', function() {
+        buffer += decoder.end();
+        
+        // Send the response
+        res.end('Hello World\n');
 
-    // Log the request path
-    console.log('Request received with these headers', headers);
+        // Log the request path
+        console.log('Request received with this payload: ', buffer);
+    })
 });
 
 // Start the server and have it listen on port 3000
